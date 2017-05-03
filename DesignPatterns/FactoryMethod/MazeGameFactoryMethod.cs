cbe7868c9d19ac50f1227736dbc13231fed69c62
Alex Shcherbakov <alexander@prolificinteractive.com>
2017-05-03 09:43:38 -0400

    first commit

---
+++ DesignPatterns/FactoryMethod/MazeGameFactoryMethod.cs
@@ -0,0 +1,72 @@
+﻿using DesignPatterns.Models;
+using DesignPatterns.Models.Bombed;
+using DesignPatterns.Models.Enchanted;
+
+namespace DesignPatterns.FactoryMethod
+{
+    public class MazeGame
+    {
+        // Factory methods
+        public virtual Maze MakeMase() => new Maze();
+        public virtual Room MakeRoom(int n) => new Room(n);
+        public virtual Wall MakeWall() => new Wall();
+        public virtual Door MakeDoor(Room r1, Room r2) => new Door(r1, r2);
+
+        public Maze CreateMaze()
+        {
+            var maze = MakeMase();
+            var room1 = MakeRoom(1);
+            var room2 = MakeRoom(2);
+            var door = MakeDoor(room1, room2);
+
+            maze.AddRoom(room1);
+            maze.AddRoom(room2);
+            room1.SetSide(Direction.North, MakeWall());
+            room1.SetSide(Direction.East, door);
+            room1.SetSide(Direction.South, MakeWall());
+            room1.SetSide(Direction.West, MakeWall());
+
+            room2.SetSide(Direction.North, MakeWall());
+            room2.SetSide(Direction.East, MakeWall());
+            room2.SetSide(Direction.South, MakeWall());
+            room2.SetSide(Direction.West, door);
+
+            return maze;
+        }
+    }
+
+    /// <summary>
+    /// Use the Factory Method pattern when
+    ///     - a class can't anticipate the class of object it creates
+    ///     - a class wants its subclasses to specify the objects it creates
+    ///     - classes delegate responsibility to one of several helper subclasses, and you want to localize
+    ///       the knowledge of which helper subclass is the delegate.
+    /// </summary>
+
+    public class BombedMazeGame : MazeGame
+    {
+        public override Wall MakeWall()
+        {
+            return new BombedWall();
+        }
+
+        public override Room MakeRoom(int n)
+        {
+            return new RoomWithBomb(n);
+        }
+    }
+
+    public class EnchantedMazeGame : MazeGame
+    {
+        public override Room MakeRoom(int n)
+        {
+            return new EnchantedRoom(n);
+        }
+
+        public override Door MakeDoor(Room r1, Room r2)
+        {
+            return new DoorNeedingSpell(r1, r2);
+        }
+    }
+
+}
---
+++ DesignPatterns/Models/Door.cs
@@ -0,0 +1,7 @@
+﻿namespace DesignPatterns.Models
+{
+    public class Door
+    {
+
+    }
+}
